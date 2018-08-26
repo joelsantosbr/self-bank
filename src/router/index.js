83d3c80505475c471c62b6d29cac25052fcd130a
Rafael Monteiro <rafaelmonteiro95@gmail.com>
2018-08-26 11:06:48 -0300

    fixed merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-<<<<<<< HEAD
-=======
 import Home from '@/components/Home';
->>>>>>> 7cb1675fc791ea9457d33a1b0e0c9123963b4e26
 import Cadastro from '@/components/Cadastro';
 import Maquininhas from '@/components/Maquininhas';
 import CadastroMaquina from '@/components/CadastroMaquina';
